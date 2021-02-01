19ebd16ec5a9f2d9e263316d86fb630302f8355e
Zigtelus <samuray911@mail.ru>
2021-02-02 02:02:03 +0300

    edits №1: main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
-function stringLength (string, maxLength) {
-  if (string.length >= Math.floor(maxLength)) {
-    console.log(string.length)
-  }
+function createRandomeNumber(firstNumber, secondNumber) {
+  let randomNumber = Math.random() * (secondNumber);
+  randomNumber < firstNumber ? randomNumber += firstNumber : randomNumber;
+  return randomNumber
 }
+
+function stringLengthFirst(minNumber, maxNumber) {
+  itRandomNumber = createRandomeNumber(minNumber, maxNumber);
+
+  return Math.floor(itRandomNumber);
+}
+
+stringLengthFirst(21, 50)
+
+
+
+
+function stringLengthSecond(minNumber, maxNumber, afterDot) {
+  itRandomNumber = createRandomeNumber(minNumber, maxNumber);
+
+  let minusNumber = String(itRandomNumber).length - (afterDot + String(Math.floor(itRandomNumber)).length + 1);
+  itRandomNumber = Number(String(itRandomNumber).slice(0, -minusNumber));
+  return itRandomNumber
+}
+
+stringLengthSecond(21, 50, 5)
