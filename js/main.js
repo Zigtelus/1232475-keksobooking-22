11c37f035ebc2eb16b5c3bee37bb71f7d4dbfa00
Zigtelus <samuray911@mail.ru>
2021-02-11 23:48:29 +0300

    third lesson

--- js/main.js
+++ js/main.js
@@ -2,10 +2,9 @@ function createRandomeNumber(firstNumber, secondNumber) {
  let randomNumber;
  if( secondNumber < firstNumber) {
    randomNumber = secondNumber + Math.random() * (firstNumber - secondNumber+1);
- }
-  else {
+ } else {
     randomNumber = firstNumber + Math.random() * (secondNumber - firstNumber+1);
-  }
+ }
   return randomNumber
 }
 
@@ -24,6 +23,81 @@ function getDecimalNumber(minNumber, maxNumber, afterDot) {
   return Number(itRandomNumber.toFixed(afterDot))
 }
 
-for (let i = 1; i < 20; i++) {
-  console.log(getDecimalNumber(24, 19,4))
+
+
+
+// third lesson
+
+function createNewMassifForOffer(initially) {
+  let newMassif = [];
+  for (let i = 0; i < getIntegerNumber(1, initially.length); i++) {
+    newMassif[i] = initially[i];
+  }
+  return newMassif
+}
+
+
+let author = {
+  avatar: 'img/avatars/user0' + getIntegerNumber(1, 8) + '.png'
+}
+
+let offer = {
+  title: 'title_number_1',
+  address: '{{location.x}}, {{location.y}}',
+  price: 2,
+  type: 'palace',
+  rooms: 1,
+  guests: 1,
+
+  checkin: function() {
+    const massif = [
+      "12:00",
+      "13:00",
+      "14:00"
+    ];
+
+    return massif[getIntegerNumber(0, massif.length -1)]
+  },
+
+  checkout: function() {
+    const massif = [
+      "12:00",
+      "13:00",
+      "14:00"
+    ];
+
+    return massif[getIntegerNumber(0, massif.length -1)]
+  },
+
+  features:  function() {
+    const massif = [
+      "wifi",
+      "dishwasher",
+      "parking",
+      "washer",
+      "elevator",
+      "conditioner"
+    ];
+
+    return createNewMassifForOffer(massif)
+  },
+
+  description: 'dog house',
+
+  photos: function() {
+    const massif = [
+      "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
+      "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
+      "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
+    ];
+
+    return createNewMassifForOffer(massif)
+  }
+
+
+}
+
+let location = {
+  x = getDecimalNumber(35.65000, 35.70000, 5),
+  y = getDecimalNumber(139.70000, 139.80000, 5)
 }
